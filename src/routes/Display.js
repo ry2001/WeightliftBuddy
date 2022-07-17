c56495d175687c7fe7948dfbf7369b47ed29c5b7
ry2001 <89305435+ry2001@users.noreply.github.com>
2022-07-18 00:26:20 +0800

    Display page skeleton

--- src/routes/Display.js
+++ src/routes/Display.js
@@ -1,15 +1,58 @@
-import React from "react"
-import { Container, Text } from "@mantine/core";
-
+import { React, useState } from "react"
+import { Container, Text, Center, Title, Stack, UnstyledButton, Group, Select } from "@mantine/core";
 
 function Display() {
+
+  const [unitData] = useState(['kg/cal', 'lb/cal', 'kg/lb', 'lb/kg']);
+  const [fontSizeData] = useState(['small', 'medium', 'large']);
+  const [themeData] = useState(['default', 'dark', 'light']);
+
+  const currentData = {
+    unit: 'kg/cal',
+    fontSize: 'medium',
+    theme: 'default'
+  };
+
+
   return (
     <>
-      <div>
-      <h1>Display Testing!</h1>
-      </div>
+    <div>
+        <Container 
+            style={{marginTop:50}}
+        >
+            <Center>
+                <Title order={1}>Display</Title>
+            </Center> 
+        </Container>
 
-      </>
+        {/* maybe can autoselect the current?? by using use state?? */}
+        <Stack
+          justify="space-around" 
+          sx={() => ({ height: 300 })}
+          align='center'
+          style={{marginTop:50}}
+        >
+          <Select 
+            label='Units'
+            data={unitData}
+            placeholder={currentData.unit}
+          />
+
+          <Select 
+            label='Font Size'
+            data={fontSizeData}
+            placeholder={currentData.fontSize}
+          />
+
+          <Select 
+            label='Theme'
+            data={themeData}
+            placeholder={currentData.theme}
+          />
+        </Stack>
+          
+    </div>
+    </>
   ); }
 
   export default Display;
