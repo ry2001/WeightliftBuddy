2e08744bfe5bfb4fb965133434e058440c72a419
ry2001 <89305435+ry2001@users.noreply.github.com>
2022-07-17 17:34:05 +0800

    profile page skeleton

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,12 +1,74 @@
 import React from "react"
-import { Container, Text } from "@mantine/core";
+import { Container, Text, Title, Image, Center, Grid, Button } from "@mantine/core";
 
 
 function Profile() {
+
+  const data = {
+    name: "Anya Forger",
+    age: 6,
+    email: 'anya_forger@gmail.com',
+    height: '99.6 cm',
+    weight: '16 kg',
+    rm: '5 kg'
+  }
+
   return (
     <>
       <div>
-      <h1>Profile Testing!</h1>
+          <Container 
+            style={{marginTop:50}}
+          >
+            <Center>
+              <Title order={1}>Profile</Title>
+            </Center> 
+          </Container>
+
+          <Container 
+            style={{marginTop:50}}
+          >
+            <Center>
+              <Image
+                width={200}
+                height={200}
+                fit='contain'
+                radius="lg"
+                src="https://daxstreet.com/wp-content/uploads/2022/05/Anya-Forger.jpg"
+              />
+            </Center> 
+          </Container>
+              
+          <Container 
+            style={{marginTop:50}}
+          >
+            <Grid>
+              <Grid.Col span={6} align='right'>
+                <Text weight='bold'>Username:</Text>
+                <Text weight='bold'>Age:</Text>
+                <Text weight='bold'>Email:</Text>
+                <Text weight='bold'>Height:</Text>
+                <Text weight='bold'>Weight:</Text>
+                <Text weight='bold'>1RM:</Text>
+              </Grid.Col>
+              <Grid.Col span={6} align='left'>
+                <Text>{data.name}</Text>
+                <Text>{data.age}</Text>
+                <Text>{data.email}</Text>
+                <Text>{data.height}</Text>
+                <Text>{data.weight}</Text>
+                <Text>{data.rm}</Text>
+              </Grid.Col>
+            </Grid>
+          </Container>
+
+          <Container 
+            style={{marginTop:50}}
+          >
+            <Center>
+              <Button>Logout</Button>
+            </Center> 
+          </Container>
+          
       </div>
 
       </>
