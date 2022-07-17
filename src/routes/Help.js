a79cb7dfeb8dc8f123cef1b54a546e78fcbcd80d
ry2001 <89305435+ry2001@users.noreply.github.com>
2022-07-17 23:51:51 +0800

    Help page skeleton

--- src/routes/Help.js
+++ src/routes/Help.js
@@ -1,15 +1,56 @@
 import React from "react"
-import { Container, Text } from "@mantine/core";
+import { Container, Text, Center, Title, Stack, UnstyledButton, Group } from "@mantine/core";
+import { ChevronRight } from 'tabler-icons-react';
 
 
 function Help() {
   return (
     <>
-      <div>
-      <h1>Help Testing!</h1>
-      </div>
+        <div>
+            <Container 
+                style={{marginTop:50}}
+            >
+                <Center>
+                    <Title order={1}>Help</Title>
+                </Center> 
+            </Container>
 
-      </>
-  ); }
+            <Center>
+                <Stack 
+                    justify="space-around" 
+                    sx={() => ({ height: 150 })}
+                    align='center'
+                    style={{marginTop:50}}
+                    >
+                    <UnstyledButton 
+                        style={{width: '100%'}} 
+                        onClick={() => console.log('Go to FAQ page')}
+                    >
+                        <Group>
+                            <Text size="xl" weight='bold'>FAQ</Text>
+                            <ChevronRight 
+                                size={48}
+                                strokeWidth={2}
+                            />
+                        </Group>
+                    </UnstyledButton>
+        
+                    <UnstyledButton 
+                        style={{width: '100%'}} 
+                        onClick={() => console.log('Go to Contact Us page')}
+                    >
+                        <Group>
+                            <Text size="xl" weight='bold'>Contact Us</Text>
+                            <ChevronRight 
+                                size={48}
+                                strokeWidth={2}
+                            />
+                        </Group>
+                    </UnstyledButton>
+                </Stack>
+            </Center>
+        </div>
+        </>
+); }
 
   export default Help;
