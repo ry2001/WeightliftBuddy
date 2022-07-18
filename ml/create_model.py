d5fad60e88c0c3fa33ce7efda8752ca544ea97d9
Abzzt <86580497+Abzzt@users.noreply.github.com>
2022-07-18 12:55:49 +0800

    Added feature to append data to csv

--- ml/create_model.py
+++ ml/create_model.py
@@ -3,16 +3,22 @@ import numpy as np
 
 data=pd.read_csv("./ml/data.csv", sep = ",", names=["torso_angle", "knee_angle"])
 # print(data)
+
+params = {}
 datapoints = [15,20,25,30,35,40]
 c = 0
 # decide paremeters based on data
 
 for i in datapoints:
     knee = (data[(data['torso_angle'] > i-1) & (data['torso_angle'] < i+1)]).loc[:, ('knee_angle')]
-    if len(knee)>5:
+    if len(knee) > 5:
         mean = np.mean(knee)
         std = np.std(knee)
         lower_bound = mean-std
         upper_bound = mean+std
-        print(f"{datapoints[c]}: {lower_bound} , {mean}, {upper_bound}")
+        # print(f"{datapoints[c]}: {lower_bound} , {mean}, {upper_bound}")
+        params[i] = (lower_bound, upper_bound)
     c+=1
+
+print(params)
+
