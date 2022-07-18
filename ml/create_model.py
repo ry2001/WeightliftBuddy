b06da9bf2b9e61a1d0db4a355229798cd51cd33f
Abzzt <86580497+Abzzt@users.noreply.github.com>
2022-07-18 12:21:03 +0800

    i rock

--- ml/create_model.py
+++ ml/create_model.py
@@ -6,7 +6,7 @@ data=pd.read_csv("./ml/data.csv", sep = ",", names=["torso_angle", "knee_angle"]
 datapoints = [15,20,25,30,35,40]
 c = 0
 # decide paremeters based on data
-
+params = {}
 for i in datapoints:
     knee = (data[(data['torso_angle'] > i-1) & (data['torso_angle'] < i+1)]).loc[:, ('knee_angle')]
     if len(knee)>5:
@@ -14,5 +14,8 @@ for i in datapoints:
         std = np.std(knee)
         lower_bound = mean-std
         upper_bound = mean+std
-        print(f"{datapoints[c]}: {lower_bound} , {mean}, {upper_bound}")
+        # print(f"{datapoints[c]}: {lower_bound} , {mean}, {upper_bound}")
+        params[i] = (lower_bound, upper_bound)
     c+=1
+    
+print(params)
