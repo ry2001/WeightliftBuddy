8af98519b675ba067c3cc878aab5eabad3dfbc86
kayweeee <90853438+kayweeee@users.noreply.github.com>
2022-07-21 18:58:09 +0800

    dk some small thing got deleted

--- src/Components/TopHeader.js
+++ src/Components/TopHeader.js
@@ -2,19 +2,25 @@ import { AppShell, Navbar, Header, Aside, Footer } from '@mantine/core';
 import { Text, Button, Center, Title} from '@mantine/core';
 import { Link} from "react-router-dom";
 import styles from './styles.module.css';
-import StickyBox from "react-sticky-box";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faHome, faUserCircle, faGear, faClock, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 function TopHeader(props) {
     const {header} =props
+    const noBackArrow = ['/home', '/history', '/createactivity', '/profile', '/']
+
+
     return(
     <>
     <AppShell
-    header={<Header fixed position={{ top: 0, left: 0, right:0}} className={styles.TopHeader} > <Center><Title>{header}</Title></Center> </Header>}>
+    header={<Header fixed position={{ top: 0, left: 0, right:0}} className={styles.TopHeader} > 
+      <ArrowBackIcon/>
+      <Center>
+        <Title>{header}</Title>
+      </Center> 
+      </Header>}>
 
 
-      </AppShell>
+    </AppShell>
 
     
     </>
